b463bc981793a6c0ff66c7112049e0dd7764d85d
SalimRaza01 <Salim.creation2017@gmail.com>
2023-12-01 10:14:20 +0530

    ios working

--- components/TaskModal.js
+++ components/TaskModal.js
@@ -7,13 +7,14 @@ import {
   Dimensions,
   Modal,
   TextInput,
-  ScrollView
+  ScrollView,
 } from 'react-native';
 import DatePicker from 'react-native-modern-datepicker';
-import { Picker } from '@react-native-picker/picker';
-import { useColorScheme } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import {useColorScheme} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 const TaskModal = ({
   modalVisible,
@@ -24,16 +25,15 @@ const TaskModal = ({
   validationError,
   assignedUser,
   setAssignedUser,
-  isEdit, 
+  isEdit,
 }) => {
-
   const colorScheme = useColorScheme();
   const isDarkTheme = colorScheme === 'dark';
 
   const dynamicStyles = {
     container: {
       // flex: 1,
-      backgroundColor: isDarkTheme ? "#000" : '#f7f7f7',
+      backgroundColor: isDarkTheme ? '#000' : '#f7f7f7',
     },
     input: {
       color: isDarkTheme ? '#FFF' : '#000',
@@ -47,27 +47,82 @@ const TaskModal = ({
       color: isDarkTheme ? '#FFFFFF' : '#333',
     },
     dateDark: {
-      backgroundColor: isDarkTheme ? "#222" : '#fff',
+      backgroundColor: isDarkTheme ? '#222' : '#fff',
       shadowColor: isDarkTheme ? '#000' : '#ccc',
     },
-  }
-
+    pickerContainer: {
+      ...Platform.select({
+        ios: {
+          borderBottomWidth: 1,
+          borderBottomColor: isDarkTheme ? '#555' : '#ccc',
+        },
+        android: {
+          borderWidth: 1,
+          borderColor: isDarkTheme ? '#555' : '#ccc',
+          borderRadius: width * 0.02,
+          backgroundColor: isDarkTheme ? '#333' : '#FFF',
+        },
+      }),
+    },
+    picker: {
+      color: isDarkTheme ? '#FFF' : '#000',
+    },
+  };
+  const renderPicker = () => {
+    if (Platform.OS === 'ios') {
+      return (
+        <RNPickerSelect
+          style={{
+            inputIOS: [styles.Pickerinput, dynamicStyles.pickerContainer],
+            inputAndroid: [styles.Pickerinput, dynamicStyles.pickerContainer],
+            iconContainer: {
+              top: 15,
+              right: 15,
+            },
+            placeholder: {
+              color: '#999',
+            },
+            ...dynamicStyles.picker,
+          }}
+          onValueChange={value => setTask({...task, priority: value})}
+          items={[
+            // {label: 'Select Priority', value: ''},
+            {label: 'High', value: 'High'},
+            {label: 'Medium', value: 'Medium'},
+            {label: 'Low', value: 'Low'},
+          ]}
+        />
+      );
+    } else {
+      return (
+        <Picker
+          style={[styles.input, dynamicStyles.pickerContainer, dynamicStyles.picker]}
+          selectedValue={task && task.priority ? task.priority : ''}
+          onValueChange={value => setTask({...task, priority: value})}>
+          <Picker.Item label="Select Priority" value="" />
+          <Picker.Item label="High" value="High" />
+          <Picker.Item label="Medium" value="Medium" />
+          <Picker.Item label="Low" value="Low" />
+        </Picker>
+      );
+    }
+  };
   return (
     <Modal visible={modalVisible} animationType="slide" transparent={false}>
       <ScrollView style={[styles.scrollview, dynamicStyles.scrollDark]}>
         <View style={[styles.modalContainer, dynamicStyles.container]}>
-        <Text style={[styles.WelcomeText, dynamicStyles.Textdark]}>
+          <Text style={[styles.WelcomeText, dynamicStyles.Textdark]}>
             {isEdit ? 'Edit Task' : 'Create Task'}
           </Text>
           <TextInput
-            style={[styles.input, { color: '#000', backgroundColor: '#fff' }]}
+            style={[styles.input, {color: '#000', backgroundColor: '#fff'}]}
             placeholderTextColor="#999"
             placeholder="Title"
             value={task && task.title ? task.title : ''}
-            onChangeText={text => setTask({ ...task, title: text })}
+            onChangeText={text => setTask({...task, title: text})}
           />
           <TextInput
-            style={[styles.input, { color: '#000', backgroundColor: '#fff' }]}
+            style={[styles.input, {color: '#000', backgroundColor: '#fff'}]}
             placeholderTextColor="#999"
             placeholder="Description"
             value={task && task.description ? task.description : ''}
@@ -80,42 +135,39 @@ const TaskModal = ({
           />
 
           <TextInput
-            style={[styles.input, { color: '#000', backgroundColor: '#fff' }]}
+            style={[styles.input, {color: '#000', backgroundColor: '#fff'}]}
             placeholder="Assign Task to"
             placeholderTextColor="#999"
             value={assignedUser}
             onChangeText={text => setAssignedUser(text)}
           />
           <Text style={styles.inputLabel}>Set Priority:</Text>
-          <Picker
-            selectedValue={task && task.priority ? task.priority : ''}
-            onValueChange={value => setTask({ ...task, priority: value })}>
-            <Picker.Item label="Select Priority" value="" />
-            <Picker.Item label="High" value="High" />
-            <Picker.Item label="Medium" value="Medium" />
-            <Picker.Item label="Low" value="Low" />
-          </Picker>
-
-          <Text style={styles.inputLabel}>Set Deadline:</Text>
+          {renderPicker()}
+          <Text style={styles.inputLabel2}>Set Deadline:</Text>
           <DatePicker
             mode="datepicker"
             selected={task && task.deadline ? task.deadline : ''}
-            onDateChange={date => setTask({ ...task, deadline: date })}
+            onDateChange={date => setTask({...task, deadline: date})}
           />
           {validationError && (
             <Text style={styles.errorText}>
               Error: Please fill in the required fields
             </Text>
           )}
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-         <TouchableOpacity
-            style={[styles.button, { backgroundColor: '#007BFF' }]}
-            onPress={() => (isEdit ? handleEditTask() : handleAddTask())}
-          >
-            <Text style={styles.buttonText}>{isEdit ? 'Update' : 'Add'}</Text>
-          </TouchableOpacity>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: height * 0.04,
+              marginBottom: height * 0.03,
+            }}>
             <TouchableOpacity
-              style={[styles.button, { backgroundColor: '#FF3B30' }]}
+              style={[styles.button, {backgroundColor: '#007BFF'}]}
+              onPress={() => (isEdit ? handleEditTask() : handleAddTask())}>
+              <Text style={styles.buttonText}>{isEdit ? 'Update' : 'Add'}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, {backgroundColor: '#FF3B30'}]}
               onPress={handleCancel}>
               <Text style={styles.buttonText}>Cancel</Text>
             </TouchableOpacity>
@@ -129,8 +181,10 @@ const TaskModal = ({
 export default TaskModal;
 
 const styles = StyleSheet.create({
-  scrollview:{
+  scrollview: {
     flex: 1,
+    marginTop: height * 0.06,
+    backgroundColor: '#FFFFFF',
   },
   container: {
     flex: 1,
@@ -221,8 +275,26 @@ const styles = StyleSheet.create({
     fontSize: width * 0.03,
     width: width * 0.86,
   },
+  Pickerinput: {
+    marginTop: height * 0.01,
+    marginLeft: width * 0.02,
+    borderWidth: 0.5,
+    borderColor: '#ccc',
+    padding: width * 0.02,
+    marginBottom: height * 0.01,
+    borderRadius: width * 0.02,
+    fontSize: width * 0.03,
+    width: width * 0.86,
+  },
   inputLabel: {
     marginTop: height * 0.01,
+    marginLeft: width * 0.02,
+    fontSize: width * 0.03,
+    fontWeight: 'bold',
+  },
+  inputLabel2: {
+    marginTop: height * 0.01,
+    marginBottom: height * 0.02,
     marginLeft: width * 0.02,
     fontSize: width * 0.03,
     fontWeight: 'bold',
